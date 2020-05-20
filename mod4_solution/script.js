03888df23e424e60e67def3413181fff7e934d3e
Mubasheer Siddiqui <siddiquimubasheer@gmail.com>
2020-05-20 11:56:30 +0530

    Update script.js

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -51,8 +51,9 @@ WARNING!!! WARNING!!!
     // to lower case character 'j' afterwards.
     // Look up these methods on Mozilla Developer Network web site if needed.
     // var firstLetter =
-    names[name] = names[name].toLowerCase();
+    
     var firstLetter = names[name].charAt(0);
+    firstLetter = firstLetter.toLowerCase();
     // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
     // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
